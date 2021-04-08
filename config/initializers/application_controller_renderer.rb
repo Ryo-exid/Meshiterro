e9a246d2fa68a8d3addb4ac7cc11e6059fc75883
Ryo-exid <sakura4oneok@gmail.com>
2021-04-08 11:18:51 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd83caa7bc610c5335df1fc68db6cce433cacbf472ac01a435167501f3404726867901577b5d4d1a38ec3c3f20ffe3357c69ded105b59154c9e9b40451541b4a3'
