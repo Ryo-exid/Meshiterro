876d6d37d560c776c263745cf50ed253242db881
Ryo-exid <sakura4oneok@gmail.com>
2021-04-09 09:42:32 +0000

    [Finish]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'post_images#index'
+  root to: 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
